f7f4010f9a4184d0956c74f05d9060d543307cbc
Paulina <paulinaa.staszynska@gmail.com>
2021-06-05 20:47:22 +0200

    Transfer media queries to style.css, deletion media_queries.css, adding media for desktop

--- js/app.js
+++ js/app.js
@@ -107,35 +107,3 @@ const app = () => {
 }
 
 app();
-
-
-
-const track = document.querySelector('.carousel__track');
-const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel__button--right');
-const prevButton = document.querySelector('.carousel__button--left');
-const dotsNav = document.querySelector('.carousel__nav');
-const dots = Array.from(dotsNav.children);
-
-const slideWidth = slides[0].getBoundingClientRect().width;
-
-// slides[0].style.left = slideWidth * 0 + 'px';
-// slides[1].style.left = slideWidth * 1 + 'px';
-// slides[2].style.left = slideWidth * 2 + 'px';
-
-
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + 'px';
-}
-
-slides.forEach(setSlidePosition);
-
-nextButton.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.current-slide');
-    const nextSlide = currentSlide.nextElementSibling;
-    const amountToMove = nextSlide.style.left;
-
-    track.style.transform = 'translateX(' + amountToMove + ')';
-
-    console.log(amountToMove);
-})
